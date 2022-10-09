4d577698efcda7925864ee2a9e36f65193a4d7a4
Michael Strauß <strauss.michael@gmx.net>
2022-10-09 07:09:40 +0200

    integrate header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StartsceenComponent } from './startsceen/startsceen.component';
+import {MatCardModule} from '@angular/material/card';
+import { HeaderComponent } from './header/header.component';
+import { MatSliderModule } from '@angular/material/slider';
 
 @NgModule({
   declarations: [
     AppComponent,
-    StartsceenComponent
+    StartsceenComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatCardModule,
+    MatSliderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
