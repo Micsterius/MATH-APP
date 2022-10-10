82e6d04e419d93cdc1b85689dc4114d149a220c5
Michael Strauß <strauss.michael@gmx.net>
2022-10-10 05:57:37 +0200

    setting get param

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -7,13 +7,30 @@ import { FormControl } from '@angular/forms';
   styleUrls: ['./settings.component.scss']
 })
 export class SettingsComponent implements OnInit {
-  fontStyleControl = new FormControl('');
+  areaOfNumbersForArithmetic: number[] = [2, 7]
+  numberOfAnswersToSolveCorrect: number = 10;
+  showPicturesForAmount: string = 'yes'; //boolean not possible because there are three options
+  mathOperator: string = 'plus';
+
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
   getOperator(operator) {
-//
+    this.mathOperator = operator;
+  }
+
+  getAreaOfNumbersForArithmetic(a, b) {
+    this.areaOfNumbersForArithmetic = [a, b];
+  }
+
+  getNumberOfAnswersToSolveCorrect(number) {
+    this.numberOfAnswersToSolveCorrect = number;
+  }
+
+  getShowPicturesForAmount(param) {
+    this.showPicturesForAmount = param;
   }
 }
