a451116a2118652c53ea7f4b3b2d68f8c68f1f4a
Michael Strauß <strauss.michael@gmx.net>
2022-10-24 06:22:04 +0200

    chat component get all docs from currentUser

--- src/app/community/chat/chat.component.ts
+++ src/app/community/chat/chat.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import tinymce from 'tinymce';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { initializeApp } from 'firebase/app';
+import { collection, getDocs, getFirestore, query, where } from 'firebase/firestore';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-chat',
@@ -7,13 +10,32 @@ import tinymce from 'tinymce';
   styleUrls: ['./chat.component.scss']
 })
 export class ChatComponent implements OnInit {
+  app = initializeApp(environment.firebase);
+  db = getFirestore(this.app);
 
-  constructor() {
+  actualUser: any;
+  allChatsId: any;
+  allChats: any;
+  constructor(
+    private firestore: AngularFirestore,) {
+    this.actualUser = JSON.parse(localStorage.getItem('user'))
+    setTimeout(() => {
+      this.loadChats()
+    }, 500);
 
-   }
+  }
 
   ngOnInit(): void {
   }
 
-  
+  async loadChats() {
+    this.allChatsId = query(collection(this.db, "posts"), where("authors", "array-contains", this.actualUser.uid));
+    this.allChats = await getDocs(this.allChatsId);
+    console.log (this.allChats)
+    this.allChats.forEach((doc) => {
+      // doc.data() is never undefined for query doc snapshots
+      console.log(doc.id, " => ", doc.data());
+    });
+
+  }
 }
