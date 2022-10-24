d5ffcb9b7d8120c0ffae85bd21d30f5fb2055a8a
Michael Strauß <strauss.michael@gmx.net>
2022-10-24 17:33:32 +0200

    error in loading user datas, two times one user

--- src/app/community/chat/chat.component.ts
+++ src/app/community/chat/chat.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { initializeApp } from 'firebase/app';
-import { collection, getDocs, getFirestore, query, where } from 'firebase/firestore';
+import { collection, doc, getDoc, getDocs, getFirestore, query, where } from 'firebase/firestore';
 import { environment } from 'src/environments/environment';
 
 @Component({
@@ -16,6 +16,8 @@ export class ChatComponent implements OnInit {
   actualUser: any;
   allChatsId: any;
   allChats: any;
+  arrayOfFriendsWithChatUid: string[] = [];
+  arrayOfFirendsWithChat: any[] = [];
   constructor(
     private firestore: AngularFirestore,) {
     this.actualUser = JSON.parse(localStorage.getItem('user'))
@@ -31,11 +33,42 @@ export class ChatComponent implements OnInit {
   async loadChats() {
     this.allChatsId = query(collection(this.db, "posts"), where("authors", "array-contains", this.actualUser.uid));
     this.allChats = await getDocs(this.allChatsId);
-    console.log (this.allChats)
     this.allChats.forEach((doc) => {
       // doc.data() is never undefined for query doc snapshots
-      console.log(doc.id, " => ", doc.data());
+      if (doc.data().authors[0] != this.actualUser.uid) this.arrayOfFriendsWithChatUid.push(doc.data().authors[0]);
+      if (doc.data().authors[1] != this.actualUser.uid) this.arrayOfFriendsWithChatUid.push(doc.data().authors[1]);
+      this.getUserInfo()
     });
+  }
 
+  async getUserInfo() {
+    this.arrayOfFirendsWithChat.length = 0;
+    this.arrayOfFriendsWithChatUid.forEach(async (uid) => {
+      let docRef = doc(this.db, "users", uid);
+      let docSnap = await getDoc(docRef);
+      if (docSnap.exists() && !this.noObjectContainsAlreadySameUid(uid)) {
+        this.arrayOfFirendsWithChat.push(docSnap.data())
+        console.log(docSnap.data()["uid"]);
+        console.log(this.arrayOfFirendsWithChat);
+      } else {
+        // doc.data() will be undefined in this case
+        console.log("No such document!");
+      }
+    })
+  }
+
+  noObjectContainsAlreadySameUid(friendUid) {
+    console.log (friendUid)
+    console.log (this.arrayOfFirendsWithChat)
+    console.log (this.arrayOfFriendsWithChatUid)
+    return this.arrayOfFirendsWithChat.some((uid) => uid == friendUid)
+  }
+
+  async getAllDocsInSubCollection(postId) {
+    let docsSnap = await getDocs(collection(this.db, "posts", postId, "text"));
+    docsSnap.forEach((doc) => {
+      console.log(doc.data());
+
+    });
   }
 }
