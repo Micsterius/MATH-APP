259d9daa9c8b038b5c320becb6f77cd8e4940937
Michael Strauß <strauss.michael@gmx.net>
2022-10-26 17:00:56 +0200

    give the right document id to the currentChat variable for read out the data in chat window

--- src/app/shared/services/chat.service.ts
+++ src/app/shared/services/chat.service.ts
@@ -16,32 +16,37 @@ export class ChatService {
   allChatsId: any;
   allChats: any;
   showChatsWithFriends: boolean = false;
-  arrayOfFriendsWithChatUid: string[] = [];
+  arrayOfFriendsWithChatUid: any[] = [];
   arrayOfFirendsWithChat: any[] = [];
   currentChatId: any = '';
 
   constructor(private router: Router) {
   }
+
   async loadChats() {
     this.actualUser = JSON.parse(localStorage.getItem('user'))
     this.allChatsId = query(collection(this.db, "posts"), where("authors", "array-contains", this.actualUser.uid));
     this.allChats = await getDocs(this.allChatsId);
     this.allChats.forEach((doc) => {
       // doc.data() is never undefined for query doc snapshots
-      if (doc.data().authors[0] != this.actualUser.uid) this.arrayOfFriendsWithChatUid.push(doc.data().authors[0]);
-      if (doc.data().authors[1] != this.actualUser.uid) this.arrayOfFriendsWithChatUid.push(doc.data().authors[1]);
+      if (doc.data().authors[0] != this.actualUser.uid) this.arrayOfFriendsWithChatUid.push({author: doc.data().authors[0], id: doc.data().id});
+      if (doc.data().authors[1] != this.actualUser.uid) this.arrayOfFriendsWithChatUid.push({author: doc.data().authors[1], id: doc.data().id});
+      console.log(doc.id, " => ", doc.data());
       this.getUserInfo()
     });
   }
 
   async getUserInfo() {
     this.arrayOfFirendsWithChat.length = 0;
-    this.arrayOfFriendsWithChatUid.forEach(async (uid) => {
-      let docRef = doc(this.db, "users", uid);
+    this.arrayOfFriendsWithChatUid.forEach(async (obj) => {
+      let docRef = doc(this.db, "users", obj.author);
       let docSnap = await getDoc(docRef);
+      let user;
       if (docSnap.exists()) {
         if (!this.arrayOfFirendsWithChat.some((friend) => friend.uid == docSnap.data()["uid"])) { //user is not already in array
-          this.arrayOfFirendsWithChat.push(docSnap.data())
+          user = docSnap.data()
+          user['id'] = obj.id; // save id of doc of chat with the user in the user object to read out the information later, when the chat window will be opened by click on the users box
+          this.arrayOfFirendsWithChat.push(user)
         }
         if (this.arrayOfFriendsWithChatUid.length == this.arrayOfFirendsWithChat.length) this.showChatsWithFriends = true;
 
@@ -50,6 +55,7 @@ export class ChatService {
         console.log("No such document!");
       }
     })
+    console.log (this.arrayOfFirendsWithChat)
   }
 
   async getAllDocsInSubCollection(postId) {
@@ -65,13 +71,19 @@ export class ChatService {
         authors: [this.actualUser.uid, friendUid],
         id: ''
       });
-      console.log(docRef.id)
       this.updateIdInFirestorePostsDocs(docRef.id)
       this.currentChatId = docRef.id;
       this.arrayOfFriendsWithChatUid.push(friendUid);
     }
-    else console.log('already doc exist');
-    this.navigateToChatWithFriend(friendUid)
+    else {
+      console.log('already doc exist');
+      this.findFriendInList(friendUid);
+    }
+  }
+
+  findFriendInList(friendUid) {
+    let friend = this.arrayOfFirendsWithChat.find((friend) => friend.uid == friendUid);
+    this.navigateToChatWithFriend(friend.id);
   }
 
   //give the id of document in the document as a field
@@ -87,29 +99,10 @@ export class ChatService {
     else return false;
   }
 
-  navigateToChatWithFriend(friendUid) {
-    this.findCurrentChatDocId(friendUid);
+
+  navigateToChatWithFriend(friendChatId) {
+    this.currentChatId = friendChatId; //Save the active doc id to read out this in the chat window
+    console.log(this.currentChatId)
     this.router.navigate(['/chat-friend']);
-  }
-
-  async findCurrentChatDocId(friendUid) {
-    let arrIdsOne = [];
-    let chatIdsOne = query(collection(this.db, "posts"), where("authors", "array-contains", this.actualUser.uid));
-    let querySnapshotOne  = await getDocs(chatIdsOne);
-    querySnapshotOne.forEach((doc) => {
-      // doc.data() is never undefined for query doc snapshots
-      arrIdsOne.push(doc.id)
-    })
-
-    let arrIdsTwo = [];
-    let chatIdsTwo = query(collection(this.db, "posts"), where("authors", "array-contains", friendUid));
-    let querySnapshotTwo  = await getDocs(chatIdsTwo);
-    querySnapshotTwo.forEach((doc) => {
-      // doc.data() is never undefined for query doc snapshots
-      arrIdsTwo.push(doc.id)
-    })
-
-    let intersection = arrIdsOne.filter((element) => arrIdsTwo.includes(element))
-    this.currentChatId = intersection;
   }
 }
