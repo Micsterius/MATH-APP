3d354e4a461d56ec1d677c13f1a65892a01a0696
Michael Strauß <strauss.michael@gmx.net>
2022-11-04 05:28:14 +0100

    fixed slider problem in settings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,3 +8,9 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'math-app';
 }
+
+/**
+ * Next Tasks
+ * create math challenge between two friends
+ * in case of add friend start a request to the oter, if he/she accepts you as a friend. Add the user to the list of the added friend
+ */
