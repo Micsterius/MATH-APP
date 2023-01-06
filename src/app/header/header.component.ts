8fd00817e2569f469ce83eb2e0b78bba5a9070b8
Michael Strauß <strauss.michael@gmx.net>
2023-01-06 14:40:31 +0100

    change code for display header image

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -13,7 +13,9 @@ export class HeaderComponent implements OnInit {
     public authService: AuthenticationService,
     public generalService: GeneralService) {
     let x = (location.pathname + location.search).substr(1)
-    if (x == null || x == undefined || x == '' || x == 'settings' || x == 'triumph') { this.generalService.inExercise = false; } //header einblenden wenn in der URL nichts steht(startscreen) oder in der url path settings oder triumph stehen
+    if (x == null || x == undefined || x == '' || x == 'settings' || x == 'triumph') this.generalService.inExercise = false;
+    else this.generalService.inExercise = true;
+     //header einblenden wenn in der URL nichts steht(startscreen) oder in der url path settings oder triumph stehen     
   }
 
   ngOnInit(): void {
