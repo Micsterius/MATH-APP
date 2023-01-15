8cfdd91eafcb29e19d5ac6cfe13e1e9db455828b
Michael Strauß <strauss.michael@gmx.net>
2023-01-15 14:16:32 +0100

    clean code, delete old one

--- src/app/shared/services/general.service.ts
+++ src/app/shared/services/general.service.ts
@@ -4,7 +4,6 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class GeneralService {
-  highlightSettingsButton: boolean = false;
   currentExercise: string = '';
   inExercise: boolean = false;
   timeStampDialogScreenSize = 0;
